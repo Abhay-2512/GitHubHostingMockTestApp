0f51c2675ffcc0ee2bee42c85e39452282fb8a9f
Abhay Bokade <abhaybokade2512@gmail.com>
2021-06-15 18:23:17 +0530

    final solve

--- src/index.js
+++ src/index.js
@@ -3,17 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 // import App02 from './CopiedData/App02';
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider >
+    
     <BrowserRouter>
       <App />
     </BrowserRouter>
-    </Provider>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
